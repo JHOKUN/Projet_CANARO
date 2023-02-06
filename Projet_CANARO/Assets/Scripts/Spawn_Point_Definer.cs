17c02c3b930effa5009acf67ed8948b1c0abaa90
JHOKUN <JNHKM10@gmail.com>
2023-02-06 23:21:12 +0100

    Problème réglé
    Un deuxième script, complètement identique a été créé, le conflit entre les deux renvoyait l'erreur

--- Projet_CANARO/Assets/Scripts/Spawn_Point_Definer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class Spawn_Point_Definer : ScriptableObject 
-{
-    public Vector2 Spawn_Point_Value; 
-}
