e89c8ad89fc15074cb8c23e2bb2d2738cca90508
JHOKUN <JNHKM10@gmail.com>
2023-02-18 01:46:26 +0100

    Conflit de commit résolu
    ne plus jamais faire ce qu'on a fait

---
+++ Projet_CANARO/Assets/Scripts/Enemy_Scripts/Enemy_Test_Movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Test_Movement : MonoBehaviour
+{
+    public float Enemy_Speed;
+    public Rigidbody2D rb;
+    public Vector2 Direction;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Direction * Enemy_Speed * Time.deltaTime);
+    }
+}
