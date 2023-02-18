a7631c1ddaea6d17d6f08d7f9e0693ba1ca8017f
JHOKUN <JNHKM10@gmail.com>
2023-02-19 00:21:14 +0100

    Fin du système de stamina en cours
    il faut encore régler quelques problèmes

--- Projet_CANARO/Assets/Scripts/Player_Scripts/Player_Movement.cs
+++ Projet_CANARO/Assets/Scripts/Player_Scripts/Player_Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,29 @@ public class Player_Movement : MonoBehaviour
     public Rigidbody2D rb;
     public Spawn_Point_Definer Start_Position;
     private bool Is_Dashing = false;
-    private bool Able_To_Dash = false;
+    private bool Able_To_Dash = true;
+    public float Max_Player_Stamina;
+    public float Current_Player_Stamina;
     public float Move_Speed = 5f;
     public float Dash_Speed = 8f;
     public float Dash_Time = 0.1f;
-    public float Dash_Cooldown = 5f;
+    public float Dash_Refill_Cooldown = 5f;
+    public float Between_Dash_Cooldown = 1f;
+    public static event Action OnPlayerDashed;
+
+
+    private IEnumerator After_Dash()
+    {
+        Able_To_Dash = false;
+        yield return new WaitForSeconds(Between_Dash_Cooldown);
+        Able_To_Dash = true;
+    }
+
+    private IEnumerator Refilling_Stamina()
+    {
+        yield return new WaitForSeconds(Dash_Refill_Cooldown);
+        Current_Player_Stamina += 1;
+    }
 
     private void Running()
     {   
@@ -21,23 +40,41 @@ public class Player_Movement : MonoBehaviour
 
     private void Dashing()
     {
-        if (Mathf.Abs(Movement.x) != Mathf.Abs(Movement.y))
+        if(Able_To_Dash == true)
+            if (Mathf.Abs(Movement.x) != Mathf.Abs(Movement.y))
+            {
+            rb.MovePosition(rb.position + Movement * Dash_Speed * Dash_Time);
+            }
+            After_Dash();
+        
+    }
+
+    private void Stamina_Refill()
+    {
+        while(Current_Player_Stamina > Max_Player_Stamina)
         {
-        rb.MovePosition(rb.position + Movement * Dash_Speed * Dash_Time);
+            Refilling_Stamina();
         }
+
     }
 
     private void Is_Dash_Starting()
     {
-        if(Input.GetKey(KeyCode.C))
+        if(Able_To_Dash == true)
         {
-            Is_Dashing = true;
+            if(Input.GetKey(KeyCode.C))
+            {
+                Is_Dashing = true;
+                Current_Player_Stamina -= 1;
+            }
         }
     }
 
     void Start()
     {
         transform.position = Start_Position.Spawn_Point_Value;
+        Current_Player_Stamina = Max_Player_Stamina;
+        OnPlayerDashed?.Invoke();
     }
 
 
@@ -47,6 +84,7 @@ public class Player_Movement : MonoBehaviour
         Movement.x = Input.GetAxisRaw("Horizontal");
         Movement.y = Input.GetAxisRaw("Vertical");
         Is_Dash_Starting();
+        Stamina_Refill();
     }
 
     void FixedUpdate()
---
+++ Projet_CANARO/Assets/Scripts/Player_Scripts/Stamina_Lightnings_Drawer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina_Lightnings_Drawer : MonoBehaviour
+{
+    public GameObject LightningPrefab;
+    public Player_Movement Movement_Player;
+    List<Stamina_System> Lightnings = new List<Stamina_System>();
+
+    public void Start()
+    {
+        Lightnings_Drawing();
+    }
+
+    public void Update()
+    {
+        Lightnings_Drawing();
+    }
+
+    private void OnEnable()
+    {
+        Player_Movement.OnPlayerDashed += Lightnings_Drawing;
+    }
+
+     private void OnDisable()
+    {
+        Player_Movement.OnPlayerDashed -= Lightnings_Drawing;
+    }
+
+    public void  Lightnings_Clearing()
+    {
+        foreach (Transform t in transform)
+        {
+            Destroy(t.gameObject);           
+        }
+            Lightnings = new List<Stamina_System>();
+    }
+
+    public void Create_Filled_Lightnings()
+    {
+        GameObject New_Lightning = Instantiate(LightningPrefab);
+        New_Lightning.transform.SetParent(transform);
+
+        Stamina_System LightningsComponent = New_Lightning.GetComponent<Stamina_System>();
+        LightningsComponent.Lightning_Image_Setting(Stamina_System.Stamina_Status.Filled);
+        Lightnings.Add(LightningsComponent);
+    }
+
+    public void Lightnings_Drawing()
+    {
+        Lightnings_Clearing();
+        int Lightnings_Needed_To_Make =(int)(Movement_Player.Max_Player_Stamina);
+        for(int i = 0; i < Lightnings_Needed_To_Make; i++)
+        {
+            Create_Filled_Lightnings();
+        }
+    }
+
+}
