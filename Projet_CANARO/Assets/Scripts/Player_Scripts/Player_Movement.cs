4faaf99bfd32417bb07d55b94b20e9c3d28bbffd
JHOKUN <JNHKM10@gmail.com>
2023-02-18 18:06:54 +0100

    Le dash est en partie fonctionnel
    le mouvement "Dash" fonctionne, il nous faut maintenant lui attribué un cooldown et un nombre d'utilisations, pour ça il va falloir travailler sur l'UI

--- Projet_CANARO/Assets/Scripts/Player_Scripts/Player_Movement.cs
+++ Projet_CANARO/Assets/Scripts/Player_Scripts/Player_Movement.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    public float Move_Speed = 5f;
+    private Vector2 Movement;
     public Rigidbody2D rb;
     public Spawn_Point_Definer Start_Position;
+    private bool Is_Dashing = false;
+    private bool Able_To_Dash = false;
+    public float Move_Speed = 5f;
+    public float Dash_Speed = 8f;
+    public float Dash_Time = 0.1f;
+    public float Dash_Cooldown = 5f;
 
-   Vector2 Movement;
+    private void Running()
+    {   
+        rb.MovePosition(rb.position + Movement * Move_Speed * Time.fixedDeltaTime);
+    }
+
+    private void Dashing()
+    {
+        if (Mathf.Abs(Movement.x) != Mathf.Abs(Movement.y))
+        {
+        rb.MovePosition(rb.position + Movement * Dash_Speed * Dash_Time);
+        }
+    }
+
+    private void Is_Dash_Starting()
+    {
+        if(Input.GetKey(KeyCode.C))
+        {
+            Is_Dashing = true;
+        }
+    }
 
     void Start()
     {
-
         transform.position = Start_Position.Spawn_Point_Value;
-
     }
 
 
@@ -23,14 +46,21 @@ public class Player_Movement : MonoBehaviour
         // ici sont stockés les inputs
         Movement.x = Input.GetAxisRaw("Horizontal");
         Movement.y = Input.GetAxisRaw("Vertical");
-    
+        Is_Dash_Starting();
     }
 
     void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + Movement * Move_Speed * Time.fixedDeltaTime);
-
-        // ici aura lieu le mouvement
+    {       // ici aura lieu le mouvement
         
+        if(Is_Dashing == false)
+        {
+            Running();
+        }
+        else
+        {
+            Dashing();
+            Is_Dashing = false;
+        }
+            
     }
 }
