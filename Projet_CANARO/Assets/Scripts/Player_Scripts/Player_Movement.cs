eb3c0d8cef85eee601a1b55cf489d1612e66b101
JHOKUN <JNHKM10@gmail.com>
2023-02-19 18:47:41 +0100

    Refill du Stamina fonctionnel
    l'énergie se recharge bien, il reste à coordonné l'affichage

--- Projet_CANARO/Assets/Scripts/Player_Scripts/Player_Movement.cs
+++ Projet_CANARO/Assets/Scripts/Player_Scripts/Player_Movement.cs
@@ -10,7 +10,7 @@ public class Player_Movement : MonoBehaviour
     public Spawn_Point_Definer Start_Position;
     private bool Is_Dashing = false;
     private bool Able_To_Dash = true;
-    //private bool Able_To_Refill = false;
+    private bool Able_To_Refill = false;
     public float Max_Player_Stamina;
     public float Current_Player_Stamina;
     public float Move_Speed = 5f;
@@ -18,7 +18,7 @@ public class Player_Movement : MonoBehaviour
     public float Dash_Time = 0.2f;
     public float Dash_Refill_Cooldown = 5f;
     public float Between_Dash_Cooldown = 1f;
-    public float Timer;
+    
     public float Tick;
     public static event Action OnPlayerDashed;
 
@@ -39,31 +39,20 @@ public class Player_Movement : MonoBehaviour
         
     }
 
-    private void Stamina_Refill()
+    private void Stamina_Refill_Countdown()
     {
         if(Current_Player_Stamina < Max_Player_Stamina)
         {
-            if(Timer == Tick)
+            if(Time.time > Tick)                
             {
-                Tick = Timer + Dash_Refill_Cooldown;
+                Tick = Time.time + Dash_Refill_Cooldown;
+                Debug.Log("ça devrait marcher");
                 Current_Player_Stamina += 1;
             }
+
         }
-
-
-
-
-       // if(Current_Player_Stamina < Max_Player_Stamina)
-        //{
-         //   Able_To_Refill = true;
-          //  while(Able_To_Refill == true)
-           // {
-           //     yield return new WaitForSeconds(Dash_Refill_Cooldown);
-           //     Current_Player_Stamina += 1;
-          //      Able_To_Refill = false;
-          //  }
-       // }
     }
+    
 
     private IEnumerator Dash()
     {
@@ -91,7 +80,6 @@ public class Player_Movement : MonoBehaviour
 
     void Awake()
     {
-        Timer = (int)Time.time;
         Tick = Dash_Refill_Cooldown;
     }
 
@@ -110,7 +98,7 @@ public class Player_Movement : MonoBehaviour
         Movement.x = Input.GetAxisRaw("Horizontal");
         Movement.y = Input.GetAxisRaw("Vertical");
         Is_Dash_Starting();
-        Stamina_Refill();
+        Stamina_Refill_Countdown();
     }
 
     void FixedUpdate()
