850b325fc14b23049851520147719091851f8b59
JHOKUN <JNHKM10@gmail.com>
2023-02-23 15:10:07 +0100

    Ajout de l'animation du bouton
    le bouton se presse lorsque le joueur marche dessus

--- Projet_CANARO/Assets/Scripts/Door_Room_Open.cs
+++ Projet_CANARO/Assets/Scripts/Door_Room_Open.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class Door_Room_Open : MonoBehaviour
 {
     public Animator Door_Animator;
-    public bool Door_Opening = false;
+    public Animator Button_Animator;
 
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            Door_Opening = true;
+            Button_Animator.SetBool("Is_Pressed", true);
             Door_Animator.SetBool("Must_Open_Door", true);
         }
     }
     
      public void Start()
     {
+        Button_Animator.SetBool("Is_Pressed", false);
         Door_Animator.SetBool("Must_Open_Door", false);
     }
 }
