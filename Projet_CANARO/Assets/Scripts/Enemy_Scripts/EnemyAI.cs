9ee7391029efc1ad1daf0aab4b16c8db31d0729a
cyprienlao <laocyprien1@gmail.com>
2023-02-25 00:23:47 +0100

    Ennemi
    L'ennemi ne traverse plus les murs et ne fait plus clip le joueur, il ne se fait pas balancer comme un curling non plus.
    On commence l'arrêt au contact du joueur, le dash pour les hauteurs est totalement fixe.

--- Projet_CANARO/Assets/Scripts/Enemy_Scripts/EnemyAI.cs
+++ Projet_CANARO/Assets/Scripts/Enemy_Scripts/EnemyAI.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-
-    public float Speed;
+    public Player_Movement Movement_Player;
+    public Rigidbody2D rb;
     public Transform Target;
-    public float Minimum_Distance;
+    public Vector2 Direction;
+    public bool Able_To_Run = true;
     public float Maximum_Distance;
+    public float Minimum_Distance;
+    public float Speed;
 
-    private void Update()
+    void Running()
     {
         if (Vector2.Distance(transform.position, Target.position) > Minimum_Distance && Vector2.Distance(transform.position, Target.position) < Maximum_Distance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+            rb.MovePosition(rb.position + Direction.normalized * Speed * Time.fixedDeltaTime);
         }
-        //else BAM le coup de lance 
-        
+    }
 
+    //void OnCollisionEnter(Collider collision)
+    //{
+
+    //}
+
+    //IEnumerator Is_Colliding()
+    //{
+    //    if (Collision)
+    //}
+
+    private void Update()
+    {
+        Direction = new Vector2(Target.position.x - transform.position.x, Target.position.y - transform.position.y);
+
+        //StartCoroutine(Is_Colliding());
+        
+        if (Able_To_Run == true)
+        {
+            Running();
+        }
+
+        
     }
 }
