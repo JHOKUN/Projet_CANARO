0a0ee431d7d0ee8dc2ba7e5bc7fc956af415435d
JHOKUN <JNHKM10@gmail.com>
2023-03-21 23:59:55 +0100

    Avancée des changeurs de scènes
    elles sont pas toutes encore là

---
+++ Projet_CANARO/Assets/Scripts/Normal_Chest_Actvate.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Normal_Chest_Actvate : MonoBehaviour
+{
+    public Animator Chest_Animator;
+    public BoxCollider2D Collider;
+    public bool Is_Open = false;
+    [SerializeField] private int Key_Amount;
+    [SerializeField] private int Potion_Amount;
+    public bool Player_In_Range = false;
+    public bool Able_To_Open = false;
+
+
+    void Opening_Condition()
+    {
+        if(Player_In_Range && Input.GetKeyDown(KeyCode.H) && Is_Open == false)
+        {
+            Chest_Animator.SetTrigger("Open_Chest");
+            Able_To_Open = true;
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D Collider)
+    {
+        if(Collider.gameObject.tag == "Player")
+        {
+            Player_In_Range = true;
+        }
+    
+
+    }
+    void OnTriggerExit2D(Collider2D Collider)
+    {
+        if(Collider.gameObject.tag == "Player")
+        {
+            Player_In_Range = false;
+        }
+    }
+    
+    void OnTriggerStay2D(Collider2D Collider)
+    {
+        if(Able_To_Open)
+        {
+            Collider.gameObject.GetComponent<Inventory_Placeholder>().Add_Key_To_Inventory(Key_Amount);
+            Collider.gameObject.GetComponent<Inventory_Placeholder>().Add_Potion_To_Inventory(Potion_Amount);
+            Is_Open = true;
+        }
+    }
+
+    void Update()
+    {
+        Opening_Condition();
+    }
+}
