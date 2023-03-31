cba46bf163916ffa299655984920a665f385da64
JHOKUN <JNHKM10@gmail.com>
2023-04-01 00:40:00 +0200

    Amelioration des chests
    abandon du OnTriggerStay, c'est un peu nul comme fonction

--- Projet_CANARO/Assets/Scripts/Chest_Activate.cs
+++ Projet_CANARO/Assets/Scripts/Chest_Activate.cs
@@ -3,32 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chest_Activate : MonoBehaviour
-{   
+{
+    public GameObject Player;   
     public Animator Chest_Animator;
     public BoxCollider2D Collider;
     public bool Is_Open = false;
-    [SerializeField] private int Key_Amount;
-    [SerializeField] private int Potion_Amount;
     [SerializeField] private bool Is_Getting_Hook;
     public bool Player_In_Range = false;
-    public bool Able_To_Open = false;
 
 
-    void Opening_Condition()
-    {
-        if(Player_In_Range && Input.GetKeyDown(KeyCode.H) && Is_Open == false)
-        {
-            Chest_Animator.SetTrigger("Open_Chest");
-            Able_To_Open = true;
-        }
-    }
-
+    
 
     void OnTriggerEnter2D(Collider2D Collider)
     {
         if(Collider.gameObject.tag == "Player")
         {
             Player_In_Range = true;
+            Player = Collider.gameObject;
         }
     
 
@@ -41,20 +32,15 @@ public class Chest_Activate : MonoBehaviour
         }
     }
     
-    void OnTriggerStay2D(Collider2D Collider)
-    {
-        if(Able_To_Open)
-        {
-            Collider.gameObject.GetComponent<Inventory_Placeholder>().Hook_Getting = Is_Getting_Hook;
-            Collider.gameObject.GetComponent<Inventory_Placeholder>().Add_Key_To_Inventory(Key_Amount);
-            Collider.gameObject.GetComponent<Inventory_Placeholder>().Add_Potion_To_Inventory(Potion_Amount);
-            Is_Open = true;
-        }
-    }
 
     void Update()
     {
-        Opening_Condition();
+        if(Player_In_Range && Input.GetKeyDown(KeyCode.R) && Is_Open == false)
+        {
+            Chest_Animator.SetTrigger("Open_Chest");
+            Player.GetComponent<Inventory_Placeholder>().Hook_Getting = Is_Getting_Hook;
+            Is_Open = true;
+        }
     }
     
 }
