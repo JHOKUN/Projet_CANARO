00ce053bf6855ab4a9363a3632c0376ca2f2a0ed
cyprienlao <laocyprien1@gmail.com>
2023-04-08 20:57:41 +0200

    Début boss
    La zone de détection est faite début des déplacements

--- Projet_CANARO/Assets/Scripts/Enemy_Scripts/Boss/Boss_Main.cs
+++ Projet_CANARO/Assets/Scripts/Enemy_Scripts/Boss/Boss_Main.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class Boss_Main : MonoBehaviour
 {
-    
+    public Is_In_Boss_Room Is_In_Boss_Room; 
+    public Rigidbody2D rb;
+    public Vector2 Direction;
+    public bool Able_To_Run;
+    public float Speed;
+
+    void Start()
+    {
+        Able_To_Run = false;
+        Speed = 3f;
+    }
+
+    void Update()
+    {
+        if (Is_In_Boss_Room.Is_In_Boss_Room_Bool)
+        {
+            Able_To_Run = true;
+        }
+        
+        if (Able_To_Run)
+        {
+            Running();
+        }
+
+        Direction = new Vector2(Is_In_Boss_Room.Player.transform.position.x - transform.position.x, Is_In_Boss_Room.Player.transform.position.y - transform.position.y);
+    }
+
+    void Running()
+    {
+        rb.MovePosition(rb.position + Direction.normalized * Speed * Time.fixedDeltaTime);
+    }
 }
--- Projet_CANARO/Assets/Scripts/Enemy_Scripts/Boss/Is_In_Boss_Room.cs
+++ Projet_CANARO/Assets/Scripts/Enemy_Scripts/Boss/Is_In_Boss_Room.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Is_In_Boss_Room : MonoBehaviour
 {
-    public GameObject Player;
+    public bool Is_In_Boss_Room_Bool;
+    public GameObject Player; 
+
+    void Start()
+    {
+        Is_In_Boss_Room_Bool = false;
+    }
+    
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            Is_In_Boss_Room_Bool = true;
             Player = other.gameObject;
-            Debug.Log("Player");
         }
     }
 }
