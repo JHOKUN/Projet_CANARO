0b034bcdfcd8d082f9e331c8ac952ca525650d54
JHOKUN <JNHKM10@gmail.com>
2023-04-16 18:41:40 +0200

    début de l'audio
    et d'autres trucs

--- Projet_CANARO/Assets/Scripts/Scene_Change.cs
+++ Projet_CANARO/Assets/Scripts/Scene_Change.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 public class Scene_Change : MonoBehaviour
 {
     public string Scene_To_Load;
-    public Vector2 Player_Position;
     public Vector2 Position_To_Load;
-    public Spawn_Point_Definer Player_Position_Storage;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,7 +18,6 @@ public class Scene_Change : MonoBehaviour
 
     IEnumerator Changing_Scene(Collider2D other)
     {
-        Player_Position_Storage.Spawn_Point_Value = Player_Position;
         other.GetComponent<Inventory_Placeholder>().Must_Fade = true;
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(Scene_To_Load);
